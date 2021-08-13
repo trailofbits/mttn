cbfa3a48033430c87e56e7ffc7dfe62d57665d65
William Woodruff <william@trailofbits.com>
2021-08-13 13:08:22 -0400

    test: make seteip nostdlib

--- test/seteip.c
+++ test/seteip.c
@@ -1,15 +1,25 @@
 /* seteip.c: set EIP from the command line
  *
- * cc -g -m32 -static -std=c99 seteip.c -o seteip.elf -mpreferred-stack-boundary=2 -fno-stack-protector -z execstack
+ *  $(CC) -g -m32 -mtune=i386 -Wl,-emain -nostdlib -static -std=c99 seteip.c \
+ *        -o seteip.elf -mpreferred-stack-boundary=2 -fno-stack-protector \
+ *        -z execstack
  */
 
-#include <string.h>
+char *strcpy(char *dst, const char *str);
 
-int main(int argc, char const *argv[]) {
+// note that argv is a raw string here, since we're in nostdlib.
+int main(int argc, char const *argv) {
   char lol[8];
 
-  // saved EIP is 16 bytes (thanks to padding) from ESP, so we write directly
+  // saved EIP is 12 bytes (thanks to padding) from ESP, so we write directly
   // here instead of expecting padding in the input
-  strcpy(lol + 16, argv[1]);
+  strcpy(lol + 12, argv);
   return 0;
+}
+
+char *strcpy(char *dst, const char *src) {
+  char *temp = dst;
+  while (*dst++ = *src++)
+    ;
+  return temp;
 }
