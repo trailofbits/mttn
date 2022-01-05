658648aee78f0a4ce9fe073e29181fa697791a77
Evan Sultanik <evan.sultanik@trailofbits.com>
2022-01-05 14:15:38 -0500

    Minimal PoC with exploit for the log4shell vuln

--- test/log4shell/vulnerable-app/java/com/trailofbits/log4shell/PoC.java
+++ test/log4shell/vulnerable-app/java/com/trailofbits/log4shell/PoC.java
@@ -7,6 +7,7 @@ class PoC {
     public static void main(String[] args) {
         Logger logger = LogManager.getLogger(PoC.class);
         for(String arg : args) {
+            System.out.println("Logging \"" + arg + "\"");
             logger.error(arg);
         }
     }
